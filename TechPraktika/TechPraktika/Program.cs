7cf4038d8df1513f32112d83af5b8b215a144c3b
Mychailo Dubrov <ipz223_dmyu@student.ztu.edu.ua>
2024-01-28 19:44:13 +0200

    Завантаження програми C# , яка рахує суму двох чисел та виводить її на екран.
    Створення коміту

---
+++ TechPraktika/TechPraktika/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Calulatoreasy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введiть перше число:");
+            double firstNumber = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введiть друге число:");
+            double secondNumber = Convert.ToDouble(Console.ReadLine());
+
+            double sum = firstNumber + secondNumber;
+            Console.WriteLine($"Сума чисел {firstNumber} i {secondNumber} дорiвнює: {sum}");
+
+            Console.ReadLine();
+        }
+    }
+}
